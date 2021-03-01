3ba94c41ad746b4aee97ad37694122116d3a31d9
Raiyan13 <tahminkabir13@gmail.com>
2021-03-01 23:48:34 +0600

    page change working fine

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,17 @@
 import React from "react";
+import { Switch, Route } from "react-router-dom";
+import Register from "./pages/auth/Register";
+import Login from "./pages/auth/Login";
+import Home from "./pages/Home";
 
-const App = () => (
-    <div>
-        <h1> Hello World </h1>
-    </div>
-);
+const App = () => {
+    return (
+        <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/register" component={Register} />
+        </Switch>
+    );
+};
 
 export default App;
