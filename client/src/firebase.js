7fd953a928b752359907f9d74a52846884491178
Raiyan13 <tahminkabir13@gmail.com>
2021-03-03 02:11:40 +0600

    firebase config changed

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -3,12 +3,12 @@ import "firebase/auth";
 
 //Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyD0Sz4PXKsfwznGmXlba0rGJX-AySc2wNc",
-    authDomain: "emedicine-65d6b.firebaseapp.com",
-    projectId: "emedicine-65d6b",
-    storageBucket: "emedicine-65d6b.appspot.com",
-    messagingSenderId: "177408270823",
-    appId: "1:177408270823:web:60c1d64dacf1a1d7285d42",
+    apiKey: "AIzaSyCodKK6MIZbrENICS0dvySYfqE55rOSsWk",
+    authDomain: "emedicine-e3e8f.firebaseapp.com",
+    projectId: "emedicine-e3e8f",
+    storageBucket: "emedicine-e3e8f.appspot.com",
+    messagingSenderId: "597008919126",
+    appId: "1:597008919126:web:ca718ee09fd7b02348e9f7",
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
