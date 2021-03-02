f2c0d916d2417aa1eb1c08c4303e53fb749e6b03
Raiyan13 <tahminkabir13@gmail.com>
2021-03-02 12:39:31 +0600

    added email sending for registration(firebase) and React-Toastify

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,4 +1,5 @@
-import * as firebase from "firebase";
+import firebase from "firebase/app";
+import "firebase/auth";
 
 //Firebase configuration
 const firebaseConfig = {
@@ -13,4 +14,4 @@ const firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 
 export const auth = firebase.auth();
-export const googleAuth = new firebase.auth.googleAuth();
+export const googleAuth = new firebase.auth.GoogleAuthProvider();
--- client/src/pages/auth/Register.js
+++ client/src/pages/auth/Register.js
@@ -1,11 +1,48 @@
 import React, { useState } from "react";
 import { Divider } from "antd";
 import ColumnGroup from "antd/lib/table/ColumnGroup";
+import { auth } from "../../firebase";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const Register = () => {
     const [email, setEmail] = useState("");
 
-    const submitHandler = () => {};
+    const submitHandler = async (e) => {
+        e.preventDefault();
+        const googleConfig = {
+            url: "http://localhost:3000/register/done",
+            // This must be true.
+            handleCodeInApp: true,
+        };
+
+        await auth.sendSignInLinkToEmail(email, googleConfig);
+        window.localStorage.setItem("emailForSignIn", email);
+        toast.success(
+            `An email has been sent to ${email}. Please check your inbox.`
+        );
+        setEmail("");
+    };
+
+    const registerForm = () => (
+        <form onSubmit={submitHandler}>
+            <input
+                type="email"
+                class="form-control text-center"
+                id="regEmail"
+                placeholder="name@example.com"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                autoFocus
+            />
+            <div class="d-grid gap-2 col-6 mx-auto mt-3">
+                <button class="btn btn-outline-primary" type="submit">
+                    Register
+                </button>
+            </div>
+        </form>
+    );
+
     return (
         <div className="d-flex p-5 justify-content-center">
             <div className="row align-items-center">
@@ -16,26 +53,8 @@ const Register = () => {
                     <h4 className="d-flex justify-content-center">
                         Get Started With Your Account
                     </h4>
-
-                    <form onSubmit={submitHandler}>
-                        <input
-                            type="email"
-                            class="form-control text-center"
-                            id="regEmail"
-                            placeholder="name@example.com"
-                            value={email}
-                            onChange={(e) => setEmail(e.target.value)}
-                            autoFocus
-                        />
-                        <div class="d-grid gap-2 col-6 mx-auto mt-3">
-                            <button
-                                class="btn btn-outline-primary"
-                                type="submit"
-                            >
-                                Register
-                            </button>
-                        </div>
-                    </form>
+                    <ToastContainer />
+                    {registerForm()}
                 </div>
             </div>
         </div>
