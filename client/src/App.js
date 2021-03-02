67b2c9da4f17f6a5168e10fb54689d8ebf9cd6f7
Hillol-Talukdar <talukdarhillol@gmail.com>
2021-03-02 21:54:32 +0600

    User can log in via registration. Hillol

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Switch, Route } from "react-router-dom";
 import Register from "./pages/auth/Register";
 import RegistrationDone from "./pages/auth/RegistrationDone";
@@ -8,7 +8,32 @@ import Header from "./components/navbar/Header";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
+import { auth } from "./firebase";
+import { useDispatch } from "react-redux";
+
 const App = () => {
+    const dispatch = useDispatch();
+
+    //to check firebase auth state
+    useEffect(() => {
+        const unsubscribe = auth.onAuthStateChanged(async (user) => {
+            if (user) {
+                const idTokenResult = await user.getIdTokenResult();
+
+                dispatch({
+                    type: "LOGGED_IN_USER",
+                    payload: {
+                        email: user.email,
+                        token: idTokenResult.token,
+                    },
+                });
+            }
+        });
+
+        // cleanup
+        return () => unsubscribe();
+    }, []);
+
     return (
         <>
             <Header />
