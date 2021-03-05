5381139c0f191cd26fd6fb14bd3b9da779c6f8f6
Raiyan13 <tahminkabir13@gmail.com>
2021-03-05 19:00:51 +0600

    implemented controller

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -2,10 +2,8 @@ const express = require("express");
 
 const router = express.Router();
 
-router.get("/user-create-or-update", (req, res) => {
-    res.json({
-        data: "This is create or update user",
-    });
-});
+const { userCreateOrUpdate } = require("../controllers/auth");
+
+router.get("/user-create-or-update", userCreateOrUpdate);
 
 module.exports = router;
