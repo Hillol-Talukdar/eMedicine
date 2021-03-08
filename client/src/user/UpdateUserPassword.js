9542068def06f9aa80e732d24d337da02528759a
Raiyan13 <tahminkabir13@gmail.com>
2021-03-08 14:20:28 +0600

    Added update password and wishlist page

---
+++ client/src/user/UpdateUserPassword.js
@@ -0,0 +1,15 @@
+import React from "react";
+import UserPageNavbar from "../../src/components/navbar/UserPageNavbar";
+
+const UpdateUserPassword = () => (
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-2">
+                <UserPageNavbar />
+            </div>
+            <div class="col">This Is Password Update Page</div>
+        </div>
+    </div>
+);
+
+export default UpdateUserPassword;
---
+++ client/src/user/Wishlist.js
@@ -0,0 +1,15 @@
+import React from "react";
+import UserPageNavbar from "../../src/components/navbar/UserPageNavbar";
+
+const Wishlist = () => (
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-2">
+                <UserPageNavbar />
+            </div>
+            <div class="col">This Is User Wishlist Page</div>
+        </div>
+    </div>
+);
+
+export default Wishlist;
