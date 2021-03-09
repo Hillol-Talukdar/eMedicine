dafc3a396d1edacc8f0c8b2e4774563741a8be89
Hillol-Talukdar <talukdarhillol@gmail.com>
2021-03-10 05:34:37 +0600

    Updated UI in admin category and admin update category page.

--- client/src/components/forms/Categoryform.js
+++ client/src/components/forms/Categoryform.js
@@ -16,24 +16,30 @@ const Categoryform = ({ submitHandler, name, setName, loading, btnName }) => (
             onChange={(e) => setName(e.target.value)}
         />
 
-        <button
-            class="btn btn-primary mt-3"
-            type="submit"
-            disabled={loading || name.length < 2}
+        <div
+            class="mx-auto mt-3"
+            data-toggle="tooltip"
+            data-placement="top"
             title="Name which contains atleast 2 character will enable this button"
         >
-            {btnName}&nbsp;&nbsp;
-            <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="16"
-                height="16"
-                fill="currentColor"
-                class="bi bi-plus-square-fill mb-1"
-                viewBox="0 0 16 16"
+            <button
+                class="btn btn-primary"
+                type="submit"
+                disabled={loading || name.length < 2}
             >
-                <path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm6.5 4.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3a.5.5 0 0 1 1 0z" />
-            </svg>
-        </button>
+                {btnName}&nbsp;&nbsp;
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="16"
+                    height="16"
+                    fill="currentColor"
+                    class="bi bi-plus-square-fill mb-1"
+                    viewBox="0 0 16 16"
+                >
+                    <path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm6.5 4.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3a.5.5 0 0 1 1 0z" />
+                </svg>
+            </button>
+        </div>
     </form>
 );
 
