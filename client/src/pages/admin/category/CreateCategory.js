55c4d5d494f51adde241a1736a56153eaeab1b8b
Raiyan13 <tahminkabir13@gmail.com>
2021-03-10 02:38:31 +0600

    Designed category show and create category page

--- client/src/pages/admin/category/CreateCategory.js
+++ client/src/pages/admin/category/CreateCategory.js
@@ -7,11 +7,20 @@ import {
     getAllCategories,
     removeACategory,
 } from "../../../functions/category";
+import { Link } from "react-router-dom";
 
 const CreateCategory = () => {
     const { user } = useSelector((state) => ({ ...state }));
     const [name, setName] = useState("");
     const [loading, setLoading] = useState(false);
+    const [categories, setCategories] = useState([]);
+
+    useEffect(() => {
+        loadAllCategories();
+    }, []);
+
+    const loadAllCategories = () =>
+        getAllCategories().then((cat) => setCategories(cat.data));
 
     const submitHandler = async (e) => {
         e.preventDefault();
@@ -84,6 +93,58 @@ const CreateCategory = () => {
                             </h4>
                         )}
                         {createCategoryForm()}
+                        <hr />
+                        <div className="d-flex flex-wrap justify-content-start">
+                            {categories.map((cat) => (
+                                <div className="m-auto">
+                                    <div
+                                        className="alert alert-primary text-dark m-3 row"
+                                        style={{ width: "18rem" }}
+                                        key={cat._id}
+                                    >
+                                        <h6 className="mt-1 col-8">
+                                            {cat.name}
+                                        </h6>
+
+                                        <span
+                                            className="btn btn-sm col-2"
+                                            style={{ float: "right" }}
+                                        >
+                                            <svg
+                                                xmlns="http://www.w3.org/2000/svg"
+                                                width="16"
+                                                height="16"
+                                                fill="currentColor"
+                                                class="bi bi-trash text-danger mb-1"
+                                                viewBox="0 0 16 16"
+                                            >
+                                                <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z" />
+                                                <path
+                                                    fill-rule="evenodd"
+                                                    d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"
+                                                />
+                                            </svg>
+                                        </span>
+                                        <Link
+                                            className="btn btn-sm col-2"
+                                            style={{ float: "right" }}
+                                            to={`/admin/category/${cat.slug}`}
+                                        >
+                                            <svg
+                                                xmlns="http://www.w3.org/2000/svg"
+                                                width="16"
+                                                height="16"
+                                                fill="currentColor"
+                                                class="bi bi-pencil mb-1"
+                                                viewBox="0 0 16 16"
+                                            >
+                                                <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5L13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175l-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z" />
+                                            </svg>
+                                        </Link>
+                                    </div>
+                                </div>
+                            ))}
+                        </div>
                     </div>
                 </div>
             </div>
