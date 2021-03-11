823e63bec9ccc732d1e723903781b54bd417c821
Hillol-Talukdar <talukdarhillol@gmail.com>
2021-03-11 18:57:40 +0600

    Product info successfully storing in database. Updated backend for creating new product form

--- client/src/pages/admin/product/CreateProduct.js
+++ client/src/pages/admin/product/CreateProduct.js
@@ -4,8 +4,7 @@ import { toast } from "react-toastify";
 import { useSelector } from "react-redux";
 import { createAProduct } from "../../../functions/product";
 import { Link } from "react-router-dom";
-import Categoryform from "../../../components/forms/Categoryform";
-import Search from "../../../components/forms/Search";
+
 
 const initState = {
     title: "",
@@ -17,7 +16,6 @@ const initState = {
     shipping: "",
     quantity: "",
     images: [],
-    // colors: ["White", "Yellow", "Red", "Green", "Blue"],
     brands: [
         "Antipyretic",
         "Tranquilizer",
@@ -26,13 +24,12 @@ const initState = {
         "Analgesic",
         "Antibiotic",
     ],
-    // color: "",
     brand: "",
 };
 
 const CreateProduct = () => {
     const [values, setValues] = useState(initState);
-    const [loading, setLoading] = useState(false);
+    // const [loading, setLoading] = useState(false);
 
     //redux
     const { user } = useSelector((state) => ({ ...state }));
@@ -48,13 +45,11 @@ const CreateProduct = () => {
         shipping,
         quantity,
         images,
-        // colors,
         brands,
-        // color,
         brand,
     } = values;
 
-    const submitHandler = (e) => {
+    const submitHandler = async (e) => {
         e.preventDefault();
         createAProduct(values, user.token)
             .then((res) => {
@@ -80,7 +75,11 @@ const CreateProduct = () => {
                 <div class="col mt-5">
                     <h1 className="text-primary text-center">eMedicine</h1>
                     <div className="mt-3">
-                        {loading ? (
+                        <h4 className="d-flex justify-content-center">
+                            Create New Product
+                        </h4>
+
+                        {/* {loading ? (
                             <h4 className="d-flex justify-content-center text-primary">
                                 Creating Product...
                             </h4>
@@ -88,7 +87,7 @@ const CreateProduct = () => {
                             <h4 className="d-flex justify-content-center">
                                 Create New Product
                             </h4>
-                        )}
+                        )} */}
                         {/* {JSON.stringify(values)}; */}
                         <form
                             class="d-flex align-items-center flex-column"
@@ -96,32 +95,27 @@ const CreateProduct = () => {
                         >
                             <input
                                 type="text"
+                                name="title"
                                 class="form-control text-center w-50"
                                 placeholder="Title"
-                                disabled={loading}
                                 value={title}
-                                required
-                                autoFocus
                                 onChange={changeHandler}
+                                autoFocus
                             />
                             <input
                                 type="text"
+                                name="description"
                                 class="form-control text-center w-50"
                                 placeholder="Description"
-                                disabled={loading}
                                 value={description}
-                                required
-                                autoFocus
                                 onChange={changeHandler}
                             />
                             <input
                                 type="number"
+                                name="price"
                                 class="form-control text-center w-50"
                                 placeholder="Product Price"
-                                disabled={loading}
                                 value={price}
-                                required
-                                autoFocus
                                 onChange={changeHandler}
                             />
                             <select
@@ -135,12 +129,10 @@ const CreateProduct = () => {
                             </select>
                             <input
                                 type="number"
+                                name="quantity"
                                 class="form-control text-center w-50"
                                 placeholder="Quantity"
-                                disabled={loading}
                                 value={quantity}
-                                required
-                                autoFocus
                                 onChange={changeHandler}
                             />
                             <select
@@ -150,7 +142,7 @@ const CreateProduct = () => {
                             >
                                 <option>Select Category</option>
                                 {brands.map((brnd) => (
-                                    <option key={brnd._id} value={brnd._id}>
+                                    <option key={brnd} value={brnd}>
                                         {brnd}
                                     </option>
                                 ))}
@@ -167,87 +159,6 @@ const CreateProduct = () => {
                                 </button>
                             </div>
                         </form>
-                        {/* <Categoryform
-                            submitHandler={submitHandler}
-                            name={name}
-                            setName={setName}
-                            loading={loading}
-                            btnName="Create Now"
-                            btnIcon={
-                                <svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    width="16"
-                                    height="16"
-                                    fill="currentColor"
-                                    class="bi bi-plus-square-fill mb-1"
-                                    viewBox="0 0 16 16"
-                                >
-                                    <path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm6.5 4.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3a.5.5 0 0 1 1 0z" />
-                                </svg>
-                            }
-                        />
-                        <hr className="mt-5" />
-                        <Search
-                            titleText={"All Categories"}
-                            keyword={keyword}
-                            setKeyword={setKeyword}
-                        />
-                        <hr className="mt-3" />
-
-                        <div className="d-flex flex-wrap justify-content-start">
-                            {categories.filter(searched(keyword)).map((cat) => (
-                                <div className="m-auto">
-                                    <div
-                                        className="alert alert-primary text-dark m-3 row"
-                                        style={{ width: "18rem" }}
-                                        key={cat._id}
-                                    >
-                                        <h6 className="mt-1 col-8">
-                                            {cat.name}
-                                        </h6>
-
-                                        <span
-                                            className="btn btn-sm col-2"
-                                            onClick={() =>
-                                                handleRemove(cat.slug)
-                                            }
-                                            style={{ float: "right" }}
-                                        >
-                                            <svg
-                                                xmlns="http://www.w3.org/2000/svg"
-                                                width="16"
-                                                height="16"
-                                                fill="currentColor"
-                                                class="bi bi-trash text-danger mb-1"
-                                                viewBox="0 0 16 16"
-                                            >
-                                                <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z" />
-                                                <path
-                                                    fill-rule="evenodd"
-                                                    d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"
-                                                />
-                                            </svg>
-                                        </span>
-                                        <Link
-                                            className="btn btn-sm col-2"
-                                            style={{ float: "right" }}
-                                            to={`/admin/category/${cat.slug}`}
-                                        >
-                                            <svg
-                                                xmlns="http://www.w3.org/2000/svg"
-                                                width="16"
-                                                height="16"
-                                                fill="currentColor"
-                                                class="bi bi-pencil mb-1"
-                                                viewBox="0 0 16 16"
-                                            >
-                                                <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5L13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175l-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z" />
-                                            </svg>
-                                        </Link>
-                                    </div>
-                                </div>
-                            ))}
-                        </div> */}
                     </div>
                 </div>
             </div>
