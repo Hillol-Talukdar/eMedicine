f192632ba988c91f1a9364121168cff7fe8d9c0e
Raiyan13 <tahminkabir13@gmail.com>
2021-03-12 15:18:12 +0600

    added image resizer for image upload form in createProduct

--- client/src/components/forms/UploadFile.js
+++ client/src/components/forms/UploadFile.js
@@ -1,8 +1,28 @@
 import React from "react";
+import Resizer from "react-image-file-resizer";
+import axios from "axios";
+import { useSelector } from "react-redux";
 
 const UploadFile = () => {
-    const uploadFileAndResize = e() => {
-        
+    const { user } = useSelector((state) => ({ ...state }));
+    const uploadFileAndResize = (e) => {
+        let files = e.target.files;
+        if (files) {
+            for (let i = 0; i < files.length; i++) {
+                Resizer.imageFileResizer(
+                    files[i],
+                    720,
+                    720,
+                    "JPEG",
+                    100,
+                    0,
+                    (uri) => {
+                        //
+                    },
+                    "base54"
+                );
+            }
+        }
     };
     return (
         <div className="row m-auto">
