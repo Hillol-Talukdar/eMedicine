969c1e6d92971fc35bf2185ff1afcafaeb508a39
Hillol-Talukdar <talukdarhillol@gmail.com>
2021-03-20 21:18:21 +0600

    Created backend for saving cart in database

---
+++ server/controllers/user.js
@@ -0,0 +1,50 @@
+const User = require("../models/user");
+const Product = require("../models/product");
+const Cart = require("../models/cart");
+
+exports.userCart = async (req, res) => {
+    const { cart } = req.body;
+    let products = [];
+
+    const user = await User.findOne({ email: req.user.email }).exec();
+
+    // check if cart with logged in user id already exists
+    let cartExistByThisUser = await Cart.findOne({ orderdBy: user._id }).exec();
+
+    if (cartExistByThisUser) {
+        cartExistByThisUser.remove();
+    }
+
+    for (let i = 0; i < cart.length; i++) {
+        let object = {};
+        object.Product = cart[i]._id;
+        object.count = cart[i].count;
+
+        //get price for creating Total
+        let { price } = await Product.findById(cart[i]._id)
+            .select("price")
+            .exec();
+
+        object.price = price;
+        products.push(object);
+    }
+
+    // console.log("products", products);
+
+    let cartTotal = 0;
+
+    for (let i = 0; i < products.length; i++) {
+        cartTotal = cartTotal + products[i].price * products[i].count;
+    }
+
+    // console.log("cartTotal", cartTotal);
+
+    let newCart = await new Cart({
+        products,
+        cartTotal,
+        orderdBy: user._id,
+    }).save();
+
+    // console.log("new Cart", newCart);
+    res.json({ ok: true });
+};
--- server/routes/user.js
+++ server/routes/user.js
@@ -2,10 +2,21 @@ const express = require("express");
 
 const router = express.Router();
 
-router.get("/user", (req, res) => {
-    res.json({
-        data: "This is user API",
-    });
-});
+//middlewares
+const { authCheck, adminCheck } = require("../middlewares/auth");
+
+//controller
+const {
+    userCart
+} = require("../controllers/user");
+
+//save cart
+router.post("/cart", authCheck, userCart);
+
+// router.get("/user", (req, res) => {
+//     res.json({
+//         data: "This is user API",
+//     });
+// });
 
 module.exports = router;
