8f62dee3accd09aab664c33948cdd74cfb6069d7
Raiyan13 <tahminkabir13@gmail.com>
2021-03-20 15:06:34 +0600

    added image on cart item table to showing the items image

--- client/src/components/cards/ProductCartCard.js
+++ client/src/components/cards/ProductCartCard.js
@@ -1,10 +1,26 @@
 import React from "react";
+import ImageModal from "react-modal-image";
+import defaultCoverImage from "../../images/defaultCoverImage.png";
 
 const ProductCartCard = ({ prod }) => {
     return (
         <tbody>
             <tr>
-                <td>Image</td>
+                <td>
+                    <div style={{ width: "120px", height: "auto" }}>
+                        {prod.images.length ? (
+                            <ImageModal
+                                small={prod.images[0].url}
+                                large={prod.images[0].url}
+                            />
+                        ) : (
+                            <ImageModal
+                                small={defaultCoverImage}
+                                large={defaultCoverImage}
+                            />
+                        )}
+                    </div>
+                </td>
                 <td>{prod.title}</td>
                 <td>{prod.price}</td>
                 <td>{prod.brand}</td>
