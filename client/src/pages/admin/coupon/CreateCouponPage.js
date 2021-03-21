0337a14c4cf00c24de15aa101e8fbdead874738b
Hillol-Talukdar <talukdarhillol@gmail.com>
2021-03-21 17:16:55 +0600

    Showed coupon at coupon page

--- client/src/pages/admin/coupon/CreateCouponPage.js
+++ client/src/pages/admin/coupon/CreateCouponPage.js
@@ -16,8 +16,13 @@ const CreateCouponPage = () => {
     const [expiry, setExpiry] = useState("");
     const [discount, setDiscount] = useState("");
     const [loading, setLoading] = useState(false);
+    const [coupons, setCoupons] = useState([]);
 
     const { user } = useSelector((state) => ({ ...state }));
+
+    useEffect(() => {
+        getCoupons().then((res) => setCoupons(res.data));
+    }, []);
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -99,6 +104,46 @@ const CreateCouponPage = () => {
                                 </button>
                             </div>
                         </form>
+
+                        <br />
+                        <h4>{coupons.length} Coupons</h4>
+
+                        <table className="table table-bordered table-striped ">
+                            <thead className="table-secondary">
+                                <tr>
+                                    <th scope="col">Name</th>
+                                    <th scope="col">Expiry</th>
+                                    <th scope="col">Discount</th>
+                                    <th scope="col">Action</th>
+                                </tr>
+                            </thead>
+
+                            <tbody>
+                                {coupons.map((c) => (
+                                    <tr key={c._id}>
+                                        <td>{c.name}</td>
+                                        <td>{new Date(c.expiry).toLocaleDateString()}</td>
+                                        <td>{c.discount}%</td>
+                                        <td>
+                                            <svg
+                                                xmlns="http://www.w3.org/2000/svg"
+                                                width="16"
+                                                height="16"
+                                                fill="currentColor"
+                                                class="bi bi-trash text-danger"
+                                                viewBox="0 0 16 16"
+                                            >
+                                                <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z" />
+                                                <path
+                                                    fill-rule="evenodd"
+                                                    d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"
+                                                />
+                                            </svg>
+                                        </td>
+                                    </tr>
+                                ))}
+                            </tbody>
+                        </table>
                     </div>
                 </div>
             </div>
