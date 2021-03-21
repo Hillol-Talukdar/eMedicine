ef0a5a62ae63d11381850d5c773e3d0c79e96121
Raiyan13 <tahminkabir13@gmail.com>
2021-03-22 02:38:27 +0600

    added stripe css and made component for stripe

---
+++ client/src/components/StripeCheckout.js
@@ -0,0 +1,4 @@
+import React, { useState, useEffect } from "react";
+import { CardElement, useStripe, useElements } from "@stripe/react-stripe-js";
+import { useSelector, useDispatch } from "react-redux";
+import { createPaymentIntent } from "../functions/stripe";
