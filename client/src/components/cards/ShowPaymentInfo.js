44edf161360c21414d8cfa1ef285a92765c6d22b
Raiyan13 <tahminkabir13@gmail.com>
2021-03-23 04:32:19 +0600

    designed changed for admin dashboard and user history page

--- client/src/components/cards/ShowPaymentInfo.js
+++ client/src/components/cards/ShowPaymentInfo.js
@@ -3,28 +3,40 @@ import React from "react";
 const ShowPaymentInfo = ({ order }) => (
     <div>
         <p>
-            <span>Order Id: {order.paymentIntent.id}</span>
-            {" / "}
-            <span>
-                Amount:{" "}
-                {(order.paymentIntent.amount /= 100).toLocaleString("en-US", {
-                    style: "currency",
-                    currency: "BDT",
-                })}
-            </span>
-            {" / "}
-            <span>Method: {order.paymentIntent.payment_method_types[0]}</span>
-            {" / "}
-            <span>Payment: {order.paymentIntent.status.toUpperCase()}</span>
-            {" / "}
-            <span>
-                Ordered on:{" "}
-                {new Date(order.paymentIntent.created * 1000).toLocaleString()}
-            </span>
-            {" / "}
-            <span className="badge bg-secondary text-white">
-                Status: {order.orderStatus}
-            </span>
+            <b>
+                <span className="badge bg-secondary mx-1">
+                    Order Id: {order.paymentIntent.id}
+                </span>
+                <span className="badge bg-primary text-white">
+                    Status: {order.orderStatus}
+                </span>
+                <span className="badge bg-success mx-1">
+                    Amount:{" "}
+                    {(order.paymentIntent.amount /= 100).toLocaleString(
+                        "en-US",
+                        {
+                            style: "currency",
+                            currency: "BDT",
+                        }
+                    )}
+                </span>
+
+                <span className="badge bg-warning text-dark mx-1">
+                    Method:{" "}
+                    {order.paymentIntent.payment_method_types[0].toUpperCase()}
+                </span>
+
+                <span className="badge bg-info text-dark mx-1">
+                    Payment: {order.paymentIntent.status.toUpperCase()}
+                </span>
+
+                <span className="badge bg-secondary mx-1">
+                    Ordered on:{" "}
+                    {new Date(
+                        order.paymentIntent.created * 1000
+                    ).toLocaleString()}
+                </span>
+            </b>
         </p>
     </div>
 );
