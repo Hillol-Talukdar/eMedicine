db57bbc9752703b720e6cb302f3d8253a0635847
Hillol-Talukdar <talukdarhillol@gmail.com>
2021-03-22 19:39:36 +0600

    card created in user history page for showing all orders and it's info

--- client/src/pages/user/History.js
+++ client/src/pages/user/History.js
@@ -19,13 +19,38 @@ const History = () => {
             setOrders(res.data);
         });
 
+    const showOrderInTable = (order) => <p>each order and it's product</p>;
+
+    const showEachOrders = () =>
+        orders.map((order, i) => (
+            <div key={i} className="m-5 p-3 card">
+                <p>Show payment info</p>
+
+                {showOrderInTable(order)}
+
+                <div className="row">
+                    <div className="col">
+                        <p>PDF Download</p>
+                    </div>
+                </div>
+            </div>
+        ));
+
     return (
         <div class="container-fluid">
             <div class="row">
                 <div class="col-md-2">
                     <UserPageNavbar />
                 </div>
-                <div class="col">This Is User History Page</div>
+                <div class="col text-center">
+                    <h4>
+                        {orders.length > 0
+                            ? "User purchase orders"
+                            : "No purchase orders"}
+                    </h4>
+
+                    {showEachOrders()}
+                </div>
             </div>
         </div>
     );
