b91f9b32664d1909f66626bd1ad3b4c95c15ebc1
Raiyan13 <tahminkabir13@gmail.com>
2021-03-22 16:48:49 +0600

    updated nav icons and made cart badge visibility conditional

--- client/src/components/navbar/Header.js
+++ client/src/components/navbar/Header.js
@@ -81,22 +81,45 @@ const Header = () => {
                 <Link to="/shop">&nbsp;&nbsp;Shop</Link>
             </Menu.Item>
             {!user && (
-                <Menu.Item
-                    key="register"
-                    icon={<UserAddOutlined />}
-                    style={{ float: "right" }}
-                >
-                    <Link to="/register">Register</Link>
+                <Menu.Item key="register" style={{ float: "right" }}>
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="16"
+                        height="16"
+                        fill="currentColor"
+                        class="bi bi-person-plus-fill mb-1"
+                        viewBox="0 0 16 16"
+                    >
+                        <path d="M1 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z" />
+                        <path
+                            fill-rule="evenodd"
+                            d="M13.5 5a.5.5 0 0 1 .5.5V7h1.5a.5.5 0 0 1 0 1H14v1.5a.5.5 0 0 1-1 0V8h-1.5a.5.5 0 0 1 0-1H13V5.5a.5.5 0 0 1 .5-.5z"
+                        />
+                    </svg>
+                    <Link to="/register">&nbsp;&nbsp;Register</Link>
                 </Menu.Item>
             )}
 
             {!user && (
-                <Menu.Item
-                    key="login"
-                    icon={<LoginOutlined />}
-                    style={{ float: "right" }}
-                >
-                    <Link to="/login">Login</Link>
+                <Menu.Item key="login" style={{ float: "right" }}>
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="17"
+                        height="17"
+                        fill="currentColor"
+                        class="bi bi-box-arrow-in-right mb-1"
+                        viewBox="0 0 16 16"
+                    >
+                        <path
+                            fill-rule="evenodd"
+                            d="M6 3.5a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-2a.5.5 0 0 0-1 0v2A1.5 1.5 0 0 0 6.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2h-8A1.5 1.5 0 0 0 5 3.5v2a.5.5 0 0 0 1 0v-2z"
+                        />
+                        <path
+                            fill-rule="evenodd"
+                            d="M11.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H1.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3z"
+                        />
+                    </svg>
+                    <Link to="/login">&nbsp;Login</Link>
                 </Menu.Item>
             )}
             {user && (
@@ -119,21 +142,70 @@ const Header = () => {
                         </svg>
                     }
                     style={{ float: "right" }}
-                    title={user.email && "  ".concat(user.email.split("@")[0])}
+                    title={user.email && " ".concat(user.email.split("@")[0])}
                 >
                     {user && user.role === "subscriber" && (
-                        <Menu.Item icon={<DashboardOutlined />}>
-                            <Link to="/user/history">Dashboard</Link>
+                        <Menu.Item>
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                width="16"
+                                height="16"
+                                fill="currentColor"
+                                class="bi bi-speedometer mb-1"
+                                viewBox="0 0 16 16"
+                            >
+                                <path d="M8 2a.5.5 0 0 1 .5.5V4a.5.5 0 0 1-1 0V2.5A.5.5 0 0 1 8 2zM3.732 3.732a.5.5 0 0 1 .707 0l.915.914a.5.5 0 1 1-.708.708l-.914-.915a.5.5 0 0 1 0-.707zM2 8a.5.5 0 0 1 .5-.5h1.586a.5.5 0 0 1 0 1H2.5A.5.5 0 0 1 2 8zm9.5 0a.5.5 0 0 1 .5-.5h1.5a.5.5 0 0 1 0 1H12a.5.5 0 0 1-.5-.5zm.754-4.246a.389.389 0 0 0-.527-.02L7.547 7.31A.91.91 0 1 0 8.85 8.569l3.434-4.297a.389.389 0 0 0-.029-.518z" />
+                                <path
+                                    fill-rule="evenodd"
+                                    d="M6.664 15.889A8 8 0 1 1 9.336.11a8 8 0 0 1-2.672 15.78zm-4.665-4.283A11.945 11.945 0 0 1 8 10c2.186 0 4.236.585 6.001 1.606a7 7 0 1 0-12.002 0z"
+                                />
+                            </svg>
+                            <Link to="/user/history">
+                                &nbsp;&nbsp;Dashboard
+                            </Link>
                         </Menu.Item>
                     )}
 
                     {user && user.role === "admin" && (
-                        <Menu.Item icon={<DashboardOutlined />}>
-                            <Link to="/admin/dashboard">Dashboard</Link>
+                        <Menu.Item>
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                width="16"
+                                height="16"
+                                fill="currentColor"
+                                class="bi bi-speedometer mb-1"
+                                viewBox="0 0 16 16"
+                            >
+                                <path d="M8 2a.5.5 0 0 1 .5.5V4a.5.5 0 0 1-1 0V2.5A.5.5 0 0 1 8 2zM3.732 3.732a.5.5 0 0 1 .707 0l.915.914a.5.5 0 1 1-.708.708l-.914-.915a.5.5 0 0 1 0-.707zM2 8a.5.5 0 0 1 .5-.5h1.586a.5.5 0 0 1 0 1H2.5A.5.5 0 0 1 2 8zm9.5 0a.5.5 0 0 1 .5-.5h1.5a.5.5 0 0 1 0 1H12a.5.5 0 0 1-.5-.5zm.754-4.246a.389.389 0 0 0-.527-.02L7.547 7.31A.91.91 0 1 0 8.85 8.569l3.434-4.297a.389.389 0 0 0-.029-.518z" />
+                                <path
+                                    fill-rule="evenodd"
+                                    d="M6.664 15.889A8 8 0 1 1 9.336.11a8 8 0 0 1-2.672 15.78zm-4.665-4.283A11.945 11.945 0 0 1 8 10c2.186 0 4.236.585 6.001 1.606a7 7 0 1 0-12.002 0z"
+                                />
+                            </svg>
+                            <Link to="/admin/dashboard">
+                                &nbsp;&nbsp;Admin Dashboard
+                            </Link>
                         </Menu.Item>
                     )}
-                    <Menu.Item icon={<LogoutOutlined />} onClick={logout}>
-                        Logout
+                    <Menu.Item onClick={logout}>
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="23"
+                            height="20"
+                            fill="currentColor"
+                            class="bi bi-box-arrow-in-left mb-1"
+                            viewBox="0 0 16 16"
+                        >
+                            <path
+                                fill-rule="evenodd"
+                                d="M10 3.5a.5.5 0 0 0-.5-.5h-8a.5.5 0 0 0-.5.5v9a.5.5 0 0 0 .5.5h8a.5.5 0 0 0 .5-.5v-2a.5.5 0 0 1 1 0v2A1.5 1.5 0 0 1 9.5 14h-8A1.5 1.5 0 0 1 0 12.5v-9A1.5 1.5 0 0 1 1.5 2h8A1.5 1.5 0 0 1 11 3.5v2a.5.5 0 0 1-1 0v-2z"
+                            />
+                            <path
+                                fill-rule="evenodd"
+                                d="M4.146 8.354a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H14.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3z"
+                            />
+                        </svg>
+                        &nbsp;Logout
                     </Menu.Item>
                 </SubMenu>
             )}
@@ -153,8 +225,10 @@ const Header = () => {
                     <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .49.598l-1 5a.5.5 0 0 1-.465.401l-9.397.472L4.415 11H13a.5.5 0 0 1 0 1H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l.84 4.479 9.144-.459L13.89 4H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" />
                 </svg>
                 <Link to="/cart">
-                    &nbsp;&nbsp;Cart&nbsp;
-                    <span className="badge bg-danger">{cart.length}</span>
+                    &nbsp;Cart&nbsp;
+                    {cart.length > 0 && (
+                        <span className="badge bg-danger">{cart.length}</span>
+                    )}
                 </Link>
             </Menu.Item>
 
